36bfb615aae3101dda84dc108aa4ade98803c2cf
mukul <mukul.kr99@gmail.com>
2021-04-28 20:37:43 +0530

    basic svm

--- svm.py
+++ svm.py
@@ -1,7 +1,6 @@
 from sklearn.model_selection import train_test_split
 from sklearn import svm
 from sklearn import metrics
-from sklearn import datasets
 
 import numpy as np
 import pandas as pd
@@ -11,7 +10,20 @@ this_dir = Path.cwd()
 
 dataset_file = this_dir / "data/pd_speech_features.ods"
 df = pd.read_excel(dataset_file, header=[0, 1], engine="odf")
+df.drop(columns=['Basic Info'], inplace=True)
 
+data = df.to_numpy(dtype=np.float32)
 
-print()
+features, labels = data[:, :-1], data[:, -1]
 
+X_train, X_test, y_train, y_test = train_test_split(features, labels, test_size=0.3, random_state=450) # 70% training and 30% test
+
+clf = svm.SVC(kernel='rbf', verbose=1) # Linear Kernel
+clf.fit(X_train, y_train)
+
+#Predict the response for test dataset
+y_pred = clf.predict(X_test)
+
+print("Accuracy:", metrics.accuracy_score(y_test, y_pred))
+print("Precision:", metrics.precision_score(y_test, y_pred))
+print("Recall:", metrics.recall_score(y_test, y_pred))
