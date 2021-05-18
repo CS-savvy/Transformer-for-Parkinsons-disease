ca80451fedaa6f58bbe8473924433f00968f3770
mukul <mukul.kr99@gmail.com>
2021-05-18 22:15:38 +0530

    exp svm

--- svm.py
+++ svm.py
@@ -2,34 +2,51 @@ from sklearn.model_selection import train_test_split
 from sklearn import svm
 from sklearn import metrics
 from sklearn.preprocessing import StandardScaler
+from sklearn.model_selection import KFold
+from sklearn.model_selection import GridSearchCV
 
 import numpy as np
 import pandas as pd
 from pathlib import Path
 
 this_dir = Path.cwd()
-
-dataset_file = this_dir / "data/pd_speech_features.ods"
-df = pd.read_excel(dataset_file, header=[0, 1], engine="odf")
-
-print("Dataset Loaded !!")
-df.drop(columns=['Basic Info'], inplace=True)
+csv_file= this_dir / "data/pd_speech_features.csv"
+df = pd.read_csv(csv_file, skiprows=[0])
+df.drop(columns=['id'], inplace=True)
+skip_column = ['gender', 'class']
+columns =list(df.columns)
+columns = [c for c in columns if c not in skip_column]
+for col in columns:
+    df[col] = (df[col] - df[col].mean())/df[col].std(ddof=0)
 
 data = df.to_numpy(dtype=np.float32)
 
 features, labels = data[:, :-1], data[:, -1]
 
-scaler = StandardScaler()
-scaler.fit(features)
-features = scaler.transform(features)
-X_train, X_test, y_train, y_test = train_test_split(features, labels, test_size=0.3, random_state=450) # 70% training and 30% test
+param_grid = { 'C':[0.1,1,100,1000],
+              'kernel':['rbf','poly','sigmoid','linear'],
+              'degree':[1,2,3,4,5,6],
+              'gamma': [1, 0.1, 0.01, 0.001, 0.0001]}
 
-clf = svm.SVC(kernel='rbf', verbose=1) # Linear Kernel
-clf.fit(X_train, y_train)
+kfold = KFold(n_splits=10, shuffle=True, random_state=450)
 
-#Predict the response for test dataset
-y_pred = clf.predict(X_test)
+accuracy = []
+precision = []
+recall = []
+for train_indexes, test_indexes in kfold.split(data):
+    X_train, y_train = features[train_indexes], labels[train_indexes]
+    X_test, y_test = features[test_indexes], labels[test_indexes]
+    clf = svm.SVC()
+    grid = GridSearchCV(clf, param_grid, n_jobs=12, cv=5, scoring='accuracy', verbose=1)
+    grid.fit(X_train, y_train)
+    print(grid.best_params_)
 
-print("Accuracy:", metrics.accuracy_score(y_test, y_pred))
-print("Precision:", metrics.precision_score(y_test, y_pred))
-print("Recall:", metrics.recall_score(y_test, y_pred))
+    y_pred = grid.predict(X_test)
+
+    accuracy.append(metrics.accuracy_score(y_test, y_pred))
+    precision.append(metrics.precision_score(y_test, y_pred))
+    recall.append(metrics.recall_score(y_test, y_pred))
+
+print("Avg accuracy:", sum(accuracy)/len(accuracy))
+print("Avg precision:", sum(precision)/len(precision))
+print("Avg recall:", sum(recall)/len(recall))
---
+++ svm_feature_selection.py
@@ -0,0 +1,56 @@
+from sklearn import svm
+from sklearn import metrics
+from sklearn.model_selection import KFold
+from mlxtend.feature_selection import SequentialFeatureSelector as SFS
+
+import numpy as np
+import pandas as pd
+from pathlib import Path
+
+this_dir = Path.cwd()
+csv_file = this_dir / "data/pd_speech_features.csv"
+df = pd.read_csv(csv_file, skiprows=[0])
+df.drop(columns=['id'], inplace=True)
+skip_column = ['gender', 'class']
+columns =list(df.columns)
+columns = [c for c in columns if c not in skip_column]
+for col in columns:
+    df[col] = (df[col] - df[col].mean())/df[col].std(ddof=0)
+
+data = df.to_numpy(dtype=np.float32)
+
+feature_names = ['gender'] + columns
+features, labels = data[:, :-1], data[:, -1]
+
+sfs = SFS(svm.SVC(), k_features=512, forward=True, floating=False, scoring='accuracy', cv=0, n_jobs=16, verbose=1)
+sfs_his = sfs.fit(features, labels, custom_feature_names=feature_names)
+selected_features= sfs_his.k_feature_names_
+print("Feature selected :", selected_features)
+feature_subset = features[:, sfs_his.k_feature_idx_]
+
+kfold = KFold(n_splits=10, shuffle=True, random_state=450)
+
+# feature_subset = features
+
+accuracy = []
+precision = []
+recall = []
+for train_indexes, test_indexes in kfold.split(data):
+    X_train, y_train = feature_subset[train_indexes], labels[train_indexes]
+    X_test, y_test = feature_subset[test_indexes], labels[test_indexes]
+
+    clf = svm.SVC(kernel='rbf', gamma=0.001, degree=1, C=100)
+    clf.fit(X_train, y_train)
+    y_pred = clf.predict(X_test)
+
+    accuracy.append(metrics.accuracy_score(y_test, y_pred))
+    precision.append(metrics.precision_score(y_test, y_pred))
+    recall.append(metrics.recall_score(y_test, y_pred))
+
+print("Accuracy: ", accuracy)
+print("Precision: ", precision)
+print("Recall: ", recall)
+
+print("Avg accuracy:", sum(accuracy)/len(accuracy))
+print("Avg precision:", sum(precision)/len(precision))
+print("Avg recall:", sum(recall)/len(recall))
