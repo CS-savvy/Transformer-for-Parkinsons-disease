63fe251ef27dadf033bf916909159385daf0bcd7
mukul <mukul.kr99@gmail.com>
2021-06-14 20:02:52 +0530

    multi thresh analysis

--- eval.py
+++ eval.py
@@ -17,15 +17,14 @@ device = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
 print("using device : ", device)
 
 
-def evaluate(model_dir, split_details=None):
-
+def evaluate(model_dir, threshold=0.5, split_details=None):
     best_model = None
-    max_val_accuracy = 0
+    max_auc_score = 0
     results = []
-    val_accuracies = []
+    val_aucs = []
     for i in range(1, 11):
         model = Transformer(config.EMBEDDING_DIM, config.ENCODER_STACK, config.ATTENTION_HEAD,
-                    dropout=config.DROPOUT, feature_length=753)
+                        dropout=config.DROPOUT, feature_length=753)
         # model = ConvModel(16, 32, 1024, 512)
         checkpoint = torch.load(model_dir / f"model_k_fold_{i}.pt")
         model.load_state_dict(checkpoint['model_state_dict'])
@@ -51,7 +50,8 @@ def evaluate(model_dir, split_details=None):
 
                 outputs = model(features)
                 pred_score = F.sigmoid(outputs)
-                preds = pred_score.round()
+                # preds = pred_score.round()
+                preds = (pred_score > threshold) * 1.0
                 y_scores.extend(list(pred_score.cpu().detach().numpy().reshape(1, -1)[0]))
                 y_preds.extend(list(preds.cpu().detach().numpy().reshape(1, -1)[0]))
                 y_labels.extend(list(labels.cpu().detach().numpy().reshape(1, -1)[0]))
@@ -61,22 +61,23 @@ def evaluate(model_dir, split_details=None):
             accuracy = tp / len(val_indices)
             precision = metrics.precision_score(y_labels, y_preds)
             recall = metrics.recall_score(y_labels, y_preds)
+            roc_auc = metrics.roc_auc_score(y_labels, y_scores)
             f1 = metrics.f1_score(y_labels, y_preds)
-            print(f"Accuracy : {accuracy} | precision : {precision} | Recall : {recall} | F1 : {f1}")
-            val_accuracies.append(accuracy)
-            if accuracy > max_val_accuracy:
-                max_val_accuracy = accuracy
+            print(f"Accuracy : {accuracy} | precision : {precision} | Recall : {recall} | F1 : {f1} | ROC : {roc_auc}")
+            val_aucs.append(roc_auc)
+            if roc_auc > max_auc_score:
+                max_auc_score = roc_auc
                 best_model = i
 
         results.extend([(u, l, p, s, l == p) for u, l, p, s in zip(all_uids, y_labels, y_preds, y_scores)])
-    print("Avg val accuracy - ", sum(val_accuracies)/ len(val_accuracies))
-    print(f"Best Model: {best_model} having accuracy {max_val_accuracy}")
+    print("Avg AUC accuracy - ", sum(val_aucs)/ len(val_aucs))
+    print(f"Best Model: {best_model} having AUC {max_auc_score}")
     test_indices = split_details['test']
     test_set = ParkinsonsDataset('data/pd_speech_features.csv', test_indices, select_feature=config.FEATURES,
                                  feature_mapping_csv='data/feature_details.csv', transform=transforms.Compose([ToTensor()]))
     test_dataloader = DataLoader(test_set, batch_size=len(test_indices), shuffle=False)
     model = Transformer(config.EMBEDDING_DIM, config.ENCODER_STACK, config.ATTENTION_HEAD,
-                dropout=config.DROPOUT, feature_length=753)
+                    dropout=config.DROPOUT, feature_length=753)
     # model = ConvModel(16, 32, 1024, 512)
     checkpoint = torch.load(model_dir / f"model_k_fold_{best_model}.pt")
     model.load_state_dict(checkpoint['model_state_dict'])
@@ -97,7 +98,8 @@ def evaluate(model_dir, split_details=None):
 
             outputs = model(features)
             pred_score = F.sigmoid(outputs)
-            preds = pred_score.round()
+            # preds = pred_score.round()
+            preds = (pred_score > threshold) * 1.0
             y_scores.extend(list(pred_score.cpu().detach().numpy().reshape(1, -1)[0]))
             y_preds.extend(list(preds.cpu().detach().numpy().reshape(1, -1)[0]))
             y_labels.extend(list(labels.cpu().detach().numpy().reshape(1, -1)[0]))
@@ -108,12 +110,14 @@ def evaluate(model_dir, split_details=None):
         precision = metrics.precision_score(y_labels, y_preds)
         recall = metrics.recall_score(y_labels, y_preds)
         f1 = metrics.f1_score(y_labels, y_preds)
-        print(f"Test - Accuracy : {accuracy} | precision : {precision} | Recall : {recall} | F1 : {f1}")
+        roc_auc = metrics.roc_auc_score(y_labels, y_scores)
+        print(f"Test - Accuracy : {accuracy} | precision : {precision} | Recall : {recall} | F1 : {f1} | ROC-AUC : {roc_auc}")
         results.extend([(u, l, p, s, l == p) for u, l, p, s in zip(all_uids, y_labels, y_preds, y_scores)])
 
     results = sorted(results, key=lambda x: x[0])
     result_df = pd.DataFrame(results, columns=["UID", 'True Label', 'Prediction', 'Score', 'Match'])
     result_df.to_csv(config.MODEL_DIR / f"eval_result.csv", index=False)
+    return accuracy
 
 
 if __name__ == "__main__":
@@ -126,4 +130,11 @@ if __name__ == "__main__":
     #                                       feature_mapping_csv='data/feature_mapping.csv',
     #                                       transform=transforms.Compose([ToTensor()]))
 
-    evaluate(config.MODEL_DIR, split_details=split_details)
+    # threshes = np.arange(0, 1, 0.05)
+    # all_accuracy = {}
+    # for i in threshes:
+    #     result = evaluate(config.MODEL_DIR, threshold=i, split_details=split_details)
+    #     all_accuracy[i] = result
+    # print(all_accuracy)
+
+    result = evaluate(config.MODEL_DIR, threshold=0.5, split_details=split_details)
